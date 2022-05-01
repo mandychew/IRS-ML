58451e00a66283ed0021634507dab9a2ed08a25a
Mandy Chew <76139009+mandychew@users.noreply.github.com>
2022-05-01 21:16:54 +0800

    Move all files to github

---
+++ Code/plot_acc_loss_graph.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+def plot_acc_loss_graph(history, epochs):
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(1, epochs + 1)
+
+    plt.plot(epochs, acc, 'b', label='Training accuracy')
+    plt.plot(epochs, val_acc, 'r', label='Validation accuracy')
+    plt.title('Training and Validation Accuracy')
+    plt.legend()
+    plt.figure()
+
+    plt.plot(epochs, loss, 'b', label='Training loss')
+    plt.plot(epochs, val_loss, 'r', label='Validation loss')
+    plt.title('Training and Validation Loss')
+    plt.legend()
+
+    plt.show()
